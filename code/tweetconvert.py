7f94d59e711a5582d153005478568e1c1439419e
Bhavesh Raheja <bhaveshraheja@gmail.com>
2013-12-02 12:24:18 -0500

    Parsed all tweets into one file, cleaned directory

--- code/tweetconvert.py
+++ code/tweetconvert.py
@@ -1,10 +1,12 @@
 class converter():
 
     def convert(self, read_file, out_file):
+        
         f = open(read_file,'rb')
         f1 = open(out_file,'wb')
 
         text = f.read()
+        
         tweets = text.split('\n')
         tweet_text = []
         
@@ -13,7 +15,9 @@ class converter():
         for tweet in tweets:
             try:
                 if not tweet == 'Authentication Complete':
-                    tweet_text.append(tweet.split(',')[2].split(':')[1].encode('UTF-8'))
+                    tweetline = tweet.split(',')[2].split(':')[1]
+                    # Remove the ' u' from the beginning of the string and the ' at the end of it
+                    tweet_text.append(tweetline[3:len(tweetline)-1])
             except IndexError:
                 print 'IndexError'
         f1.write('\n'.join(tweet_text)) 
