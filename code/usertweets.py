047aea775c1eb692371e1a65c8bbcf541dcc58c9
Bhavesh Raheja <bhaveshraheja@gmail.com>
2013-12-02 23:26:28 -0500

    Changes made to get user tweets, not tested entirely due to rate limit

--- code/usertweets.py
+++ code/usertweets.py
@@ -1,18 +1,45 @@
 '''Program that downloads the tweets of a give user'''
-import auth_details
+import auth_details as auth
 from twython import Twython
+from twython import TwythonError
+from twython import TwythonRateLimitError
+
+class TwitterException(Exception):
+    pass
 
 class tweetdownloader():
 
-    _tweetid = None
-    _twitter = None
     _tweets = []
-    def init(self):
-       self._twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+    _twitter = Twython(auth.APP_KEY, auth.APP_SECRET, auth.OAUTH_TOKEN, auth.OAUTH_TOKEN_SECRET)
 
-    def get_user_tweets(self, tweetid, count=200):
+    def get_user_tweets(self, twitterhandle, count=200):
+        count = 0
+        attempt = 0
+        tweetid = ''
         if count <= 200:
-            tweets = self._twitter.get_user_timeline(screen_name=tweetid, count=count)
+            tweets = self._twitter.get_user_timeline(screen_name=twitterhandle, count=count)
         for tweet in tweets:
             self._tweets.append(tweet['text'])
+            tweetid = tweet['id']
+            count = count + 1
+        while count < 3200 or attempt < 5:
+            furthertweets = []
+            try:
+                furthertweets.append(self.get_user_tweetsfrom(twitterhandle, tweetid))
+                count = count+len(furthertweets)
+                print "count =",count
+            except TwitterException:
+                attempt = attempt + 1
+                print "attempt =",attempt
+
+
+    def get_user_tweetsfrom(self, twitterhandle, sinceid):
+        try:
+            tweets = self._twitter.get_user_timeline(screen_name=twitterhandle, max_id=sinceid)
+        except TwythonRateLimitError,TwythonError:
+            raise TwitterException
+        for tweet in tweets:
+            self._tweets.append(tweet['text'])
+            tweetid = tweet['id']
         return self._tweets
+
